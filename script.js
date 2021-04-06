8849815670d1aaa1fdb0d5513f44191cda490e7f
Yashar Zafari <yasharez@gmail.com>
2021-04-05 17:43:32 -0700

    Implemented more functionality for buttons

--- script.js
+++ script.js
@@ -1,51 +1,60 @@
-let numbers = ['', ''];
+let total = '';
+let current = '';
 
 const display = document.getElementById('display');
 const numberButtons = document.querySelectorAll('.numbers');
 const period = document.getElementById('dot');
+const del = document.getElementById('del');
 const clear = document.getElementById('clear');
 
 numberButtons.forEach((button) => {
     button.addEventListener('click', function() {
-        numbers[0] += button.textContent;
-        display.textContent = numbers[0];
-        console.log(numbers[0])
+        current += button.textContent;
+        display.textContent = current;
     });
 });
 
 clear.addEventListener('click', function() {
-    numbers[0] = '';
+    current = '';
     display.textContent = '0';
+    period.disabled = false;
 });
 
 period.addEventListener('click', function(){
-    if(numbers[0].indexOf('.') !== -1){
-        period.disabled = true;
-    }
-    numbers[0] += period.textContent;
-    display.textContent = numbers[0];
-    console.log(numbers[0]);
+    period.disabled = true;
+    current += period.textContent;
+    display.textContent = current;
 });
-function add (array){
-    num1 = Number(array[0]);
-    num2 = Number(array[1]);
+
+del.addEventListener('click', function(){
+    let deleted = current.slice(current.length - 1);
+    current = current.slice(0, current.length - 1)
+    display.textContent = current;
+    if(deleted === '.'){
+        period.disabled = false;
+    };
+    console.log(current, deleted);
+});
+function add (num1, num2){
+    num1 = Number(num1);
+    num2 = Number(num2);
     return num1 + num2;
 }
 
 function subtract (array){
-    num1 = Number(array[0]);
-    num2 = Number(array[1]);
+    num1 = Number(num1);
+    num2 = Number(num2);
     return num1 - num2;
 }
 
 function multiply (array){
-    num1 = Number(array[0]);
-    num2 = Number(array[1]);
+    num1 = Number(num1);
+    num2 = Number(num2);
     return num1 * num2;
 }
 
 function divide (array){
-    num1 = Number(array[0]);
-    num2 = Number(array[1]);
+    num1 = Number(num1);
+    num2 = Number(num2);
     return num1 / num2;
 }
