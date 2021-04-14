3f16bdb99189d8e6cba3b5c4f99c84a411586a81
Yashar Zafari <yasharez@gmail.com>
2021-04-13 19:14:47 -0700

    Major code overhaul, have to work through a lot of the functionality again

--- script.js
+++ script.js
@@ -6,7 +6,8 @@
 let total = '';
 let current = '';
 let result;
-let previousOperator = '';
+let operation = '';
+let screenReset = false;
 
 // Grab the needed DOM elements
 const display = document.getElementById('display');
@@ -17,80 +18,74 @@ const clear = document.getElementById('clear');
 const sign = document.getElementById('sign');
 const operators = document.querySelectorAll('.operators');
 const equals = document.getElementById('equals');
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        console.log(`total: ${total}, current: ${current}, operator: ${operation}`)
+    });
+});
 
 // Populate the display and 'current' variable with numbers inputted by user
 numberButtons.forEach((button) => {
     button.addEventListener('click', function() {
-        current += button.textContent;
-        display.textContent = current;
-        console.log(`number button log: total: ${total}, current: ${current}`);
+        if(screenReset) resetScreen();
+        if(display.textContent === '0'){
+            display.textContent = '';
+        }
+        display.textContent += button.textContent;
     });
-});
-
-// Function that clears display and 'current' variable
-clear.addEventListener('click', function() {
-    current = '';
-    total = '';
-    display.textContent = '0';
-    period.disabled = false;
-});
-
-// Function that adds a decimal to display/'current' variable
-period.addEventListener('click', function(){
-    period.disabled = true;
-    current += period.textContent;
-    display.textContent = current;
-});
-
-// Function that removes last display entry
-del.addEventListener('click', function(){
-    let deleted = current.slice(current.length - 1);
-    current = current.slice(0, current.length - 1)
-    display.textContent = current;
-    if(deleted === '.'){
-        period.disabled = false;
-    };
-});
-
-// Function to change sign of 'current' variable
-sign.addEventListener('click', function(){
-    if(current.length > 0){
-        if(current.slice(0,1) !== '-'){
-            current = '-' + current;
-            display.textContent = current;
-        }else if(current.slice(0,1) === '-'){
-            current = current.slice(1);
-            display.textContent = current;
-        };
-    };
 });
 
 // Main Code to operate on numbers
 operators.forEach((button) => {
     button.addEventListener('click', function() {
-        if(total === ''){
-            total = current;
-            current = '';
-            display.textContent = total;
-            equals.addEventListener('click', function(){
-                result = operate(button.textContent, total, current);
-                display.textContent = result;
-                total = result;
-                current = '';
-            });
+        operation = button.textContent;
+        if (total === ''){
+            total = display.textContent;
+            screenReset = true;
         }else{
-            if(previousOperator !== button.textContent){
-                result = operate(previousOperator, total, current);
-            }else{
-                result = operate(button.textContent, total, current);
-            };
-            display.textContent = result;
-            total = result;
-            current = '';
-        };
-        previousOperator = button.textContent;
+            current = display.textContent;
+            total = operate(operation, total, current);
+            display.textContent = total;
+            console.log(operation);
+            operation = '';
+            screenReset = true;
+        }
+        console.log(operation);
     });
 });
+
+equals.addEventListener('click', function(){
+    current = display.textContent;
+    if(total !== '' && current !== ''){
+        display.textContent = operate(operation, total, current);
+        screenReset = true;
+        operation = '';
+    };
+});
+
+// if(total === ''){
+//     total = current;
+//     current = '';
+//     display.textContent = total;
+//     equals.addEventListener('click', function(){
+//         result = operate(button.textContent, total, current);
+//         display.textContent = result;
+//         total = result;
+//         current = '';
+//     });
+// }else{
+//     if(operation !== button.textContent){
+//         result = operate(operation, total, current);
+//     }else{
+//         result = operate(button.textContent, total, current);
+//     };
+//     display.textContent = result;
+//     total = result;
+//     current = '';
+// };
+// operation = button.textContent;
 
 // Function that outputs the result of two entries and operator
 function operate(operator, num1, num2){
@@ -110,7 +105,46 @@ function operate(operator, num1, num2){
         case '\u00D7':
             num1 = Number(num1);
             num2 = Number(num2);
-            return String(num1 / num2);
+            return String(num1 * num2);
     };
 };
 
+// Function that clears display and 'current' variable NEEDS WORK
+clear.addEventListener('click', function() {
+    display.textContent = '0';
+    period.disabled = false;
+});
+
+// Function that adds a decimal to display/'current' variable NEEDS WORK
+period.addEventListener('click', function(){
+    period.disabled = true;
+    current += period.textContent;
+    display.textContent = current;
+});
+
+// Function that removes last display entry NEED TO CHECK
+del.addEventListener('click', function(){
+    let deleted = display.textContent.slice(display.textContent.length - 1);
+    display.textContent = display.textContent.slice(0, display.textContent.length - 1);
+    if(deleted === '.'){
+        period.disabled = false;
+    };
+});
+
+// Function to change sign of 'current' variable NEEDS WORK
+sign.addEventListener('click', function(){
+    if(display.length > 0){
+        if(current.slice(0,1) !== '-'){
+            current = '-' + current;
+            display.textContent = current;
+        }else if(current.slice(0,1) === '-'){
+            current = current.slice(1);
+            display.textContent = current;
+        };
+    };
+});
+
+function resetScreen(){
+    display.textContent = '0';
+    screenReset = false;
+}
